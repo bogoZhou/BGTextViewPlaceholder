bd7f2d63a8027533dc96472d88a1ec4bc5d63255
周博 <bogo_zhou@sina.com>
2016-12-30 14:52:21 +0800

    textView+placeholder控件
    么有描述

---
+++ BGTextViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BGTextViewDemo
+//
+//  Created by 周博 on 16/12/30.
+//  Copyright © 2016年 BogoZhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
